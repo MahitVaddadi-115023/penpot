MahitVaddadi-115023/penpot

commit 5e3ed126865da8a4a229e5673d07bd3a534249c5

:recycle: Refactor auth code

backend/src/app/rpc/commands/ldap.clj
@@ -1,0 +1,75 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) UXBOX Labs SL
+
+(ns app.rpc.commands.ldap
+  (:require
+   [app.auth.ldap :as ldap]
+   [app.common.exceptions :as ex]
+   [app.common.spec :as us]
+   [app.db :as db]
+   [app.loggers.audit :as-alias audit]
+   [app.rpc.commands.auth :as cmd.auth]
+   [app.rpc.queries.profile :as profile]
+   [app.util.services :as sv]
+   [clojure.spec.alpha :as s]))
+
+;; --- COMMAND: login-with-ldap
+
+(declare login-or-register)
+
+(s/def ::email ::us/email)
+(s/def ::password ::us/string)
+(s/def ::invitation-token ::us/string)
+
+(s/def ::login-with-ldap
+  (s/keys :req-un [::email ::password]
+          :opt-un [::invitation-token]))
+
+(sv/defmethod ::login-with-ldap {:auth false}
+  [{:keys [session tokens ldap] :as cfg} params]
+  (when-not ldap
+    (ex/raise :type :restriction
+              :code :ldap-not-initialized
+              :hide "ldap auth provider is not initialized"))
+
+  (let [info (ldap/authenticate ldap params)]
+    (when-not info
+      (ex/raise :type :validation
+                :code :wrong-credentials))
+
+    (let [profile (login-or-register cfg info)]
+      (if-let [token (:invitation-token params)]
+        ;; If invitation token comes in params, this is because the
+        ;; user comes from team-invitation process; in this case,
+        ;; regenerate token and send back to the user a new invitation
+        ;; token (and mark current session as logged).
+        (let [claims (tokens :verify {:token token :iss :team-invitation})
+              claims (assoc claims
+                            :member-id  (:id profile)
+                            :member-email (:email profile))
+              token  (tokens :generate claims)]
+          (with-meta {:invitation-token token}
+            {:transform-response ((:create session) (:id profile))
+             ::audit/props (:props profile)
+             ::audit/profile-id (:id profile)}))
+
+        (with-meta profile
+          {:transform-response ((:create session) (:id profile))
+           ::audit/props (:props profile)
+           ::audit/profile-id (:id profile)})))))
+
+(defn- login-or-register
+  [{:keys [pool] :as cfg} info]
+  (db/with-atomic [conn pool]
+    (or (some->> (:email info)
+                 (profile/retrieve-profile-data-by-email conn)
+                 (profile/populate-additional-data conn)
+                 (profile/decode-profile-row))
+        (->> (assoc info :is-active true :is-demo false)
+             (cmd.auth/create-profile conn)
+             (cmd.auth/create-profile-relations conn)
+             (profile/strip-private-attrs)))))
+

backend/src/app/auth/ldap.clj
@@ -1,0 +1,137 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) UXBOX Labs SL
+
+(ns app.auth.ldap
+  (:require
+   [app.common.exceptions :as ex]
+   [app.common.logging :as l]
+   [app.common.spec :as us]
+   [app.config :as cf]
+   [clj-ldap.client :as ldap]
+   [clojure.spec.alpha :as s]
+   [clojure.string]
+   [integrant.core :as ig]))
+
+(defn- prepare-params
+  [cfg]
+  {:ssl?      (:ssl cfg)
+   :startTLS? (:tls cfg)
+   :bind-dn   (:bind-dn cfg)
+   :password  (:bind-password cfg)
+   :host      {:address (:host cfg)
+               :port    (:port cfg)}})
+
+(defn- connect
+  "Connects to the LDAP provider and returns a connection. An
+  exception is raised if no connection is possible."
+  ^java.lang.AutoCloseable
+  [cfg]
+  (try
+    (-> cfg prepare-params ldap/connect)
+    (catch Throwable cause
+      (ex/raise :type :restriction
+                :code :unable-to-connect-to-ldap
+                :hint "unable to connect to ldap server"
+                :cause cause))))
+
+(defn- replace-several [s & {:as replacements}]
+  (reduce-kv clojure.string/replace s replacements))
+
+(defn- search-user
+  [{:keys [conn attrs base-dn] :as cfg} email]
+  (let [query   (replace-several (:query cfg) ":username" email)
+        params  {:filter query
+                 :sizelimit 1
+                 :attributes attrs}]
+    (first (ldap/search conn base-dn params))))
+
+(defn- retrieve-user
+  [{:keys [conn] :as cfg} {:keys [email password]}]
+  (when-let [{:keys [dn] :as user} (search-user cfg email)]
+    (when (ldap/bind? conn dn password)
+      {:fullname (get user (-> cfg :attrs-fullname keyword))
+       :email    email
+       :backend  "ldap"})))
+
+(s/def ::fullname ::us/not-empty-string)
+(s/def ::email ::us/email)
+(s/def ::backend ::us/not-empty-string)
+
+(s/def ::info-data
+  (s/keys :req-un [::fullname ::email ::backend]))
+
+(defn authenticate
+  [cfg params]
+  (with-open [conn (connect cfg)]
+    (when-let [user (-> (assoc cfg :conn conn)
+                        (retrieve-user params))]
+      (when-not (s/valid? ::info-data user)
+        (let [explain (s/explain-str ::info-data user)]
+          (l/warn ::l/raw (str "invalid response from ldap, looks like ldap is not configured correctly\n" explain))
+          (ex/raise :type :restriction
+                    :code :wrong-ldap-response
+                    :explain explain)))
+      user)))
+
+(defn- try-connectivity
+  [cfg]
+  ;; If we have ldap parameters, try to establish connection
+  (when (and (:bind-dn cfg)
+             (:bind-password cfg)
+             (:host cfg)
+             (:port cfg))
+    (try
+      (with-open [_ (connect cfg)]
+        (l/info :hint "provider initialized"
+                :provider "ldap"
+                :host (:host cfg)
+                :port (:port cfg)
+                :tls? (:tls cfg)
+                :ssl? (:ssl cfg)
+                :bind-dn (:bind-dn cfg)
+                :base-dn (:base-dn cfg)
+                :query (:query cfg))
+        cfg)
+      (catch Throwable cause
+        (l/error :hint "unable to connect to LDAP server (LDAP auth provider disabled)"
+                 :host (:host cfg) :port (:port cfg) :cause cause)
+        nil))))
+
+(defn- prepare-attributes
+  [cfg]
+  (assoc cfg :attrs [(:attrs-username cfg)
+                     (:attrs-email cfg)
+                     (:attrs-fullname cfg)]))
+
+(defmethod ig/init-key ::provider
+  [_ cfg]
+  (when (:enabled? cfg)
+    (some-> cfg try-connectivity prepare-attributes)))
+
+(s/def ::enabled? ::us/boolean)
+(s/def ::host ::cf/ldap-host)
+(s/def ::port ::cf/ldap-port)
+(s/def ::ssl ::cf/ldap-ssl)
+(s/def ::tls ::cf/ldap-starttls)
+(s/def ::query ::cf/ldap-user-query)
+(s/def ::base-dn ::cf/ldap-base-dn)
+(s/def ::bind-dn ::cf/ldap-bind-dn)
+(s/def ::bind-password ::cf/ldap-bind-password)
+(s/def ::attrs-email ::cf/ldap-attrs-email)
+(s/def ::attrs-fullname ::cf/ldap-attrs-fullname)
+(s/def ::attrs-username ::cf/ldap-attrs-username)
+
+(defmethod ig/pre-init-spec ::provider
+  [_]
+  (s/keys :opt-un [::host ::port
+                   ::ssl ::tls
+                   ::enabled?
+                   ::bind-dn
+                   ::bind-password
+                   ::query
+                   ::attrs-email
+                   ::attrs-username
+                   ::attrs-fullname]))

backend/src/app/rpc/mutations/ldap.clj
@@ -1,141 +1,0 @@
-;; This Source Code Form is subject to the terms of the Mozilla Public
-;; License, v. 2.0. If a copy of the MPL was not distributed with this
-;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
-;;
-;; Copyright (c) UXBOX Labs SL
-
-(ns app.rpc.mutations.ldap
-  (:require
-   [app.common.exceptions :as ex]
-   [app.common.logging :as l]
-   [app.common.spec :as us]
-   [app.config :as cfg]
-   [app.db :as db]
-   [app.loggers.audit :as audit]
-   [app.rpc.mutations.profile :as profile-m]
-   [app.rpc.queries.profile :as profile-q]
-   [app.util.services :as sv]
-   [clj-ldap.client :as ldap]
-   [clojure.spec.alpha :as s]
-   [clojure.string]))
-
-
-(s/def ::fullname ::us/not-empty-string)
-(s/def ::email ::us/email)
-(s/def ::backend ::us/not-empty-string)
-
-(s/def ::info-data
-  (s/keys :req-un [::fullname ::email ::backend]))
-
-(defn connect
-  ^java.lang.AutoCloseable
-  []
-  (let [params {:ssl?      (cfg/get :ldap-ssl)
-                :startTLS? (cfg/get :ldap-starttls)
-                :bind-dn   (cfg/get :ldap-bind-dn)
-                :password  (cfg/get :ldap-bind-password)
-                :host      {:address (cfg/get :ldap-host)
-                            :port    (cfg/get :ldap-port)}}]
-    (try
-      (ldap/connect params)
-      (catch Exception e
-        (ex/raise :type :restriction
-                  :code :ldap-disabled
-                  :hint "ldap disabled or unable to connect"
-                  :cause e)))))
-
-;; --- Mutation: login-with-ldap
-
-(declare authenticate)
-(declare login-or-register)
-
-(s/def ::email ::us/email)
-(s/def ::password ::us/string)
-(s/def ::invitation-token ::us/string)
-
-(s/def ::login-with-ldap
-  (s/keys :req-un [::email ::password]
-          :opt-un [::invitation-token]))
-
-(sv/defmethod ::login-with-ldap {:auth false}
-  [{:keys [pool session tokens] :as cfg} params]
-  (db/with-atomic [conn pool]
-    (let [info (authenticate params)
-          cfg  (assoc cfg :conn conn)]
-
-      (when-not info
-        (ex/raise :type :validation
-                  :code :wrong-credentials))
-
-      (when-not (s/valid? ::info-data info)
-        (let [explain (s/explain-str ::info-data info)]
-          (l/warn ::l/raw (str "invalid response from ldap, looks like ldap is not configured correctly\n" explain))
-          (ex/raise :type :restriction
-                    :code :wrong-ldap-response
-                    :reason explain)))
-
-      (let [profile (login-or-register cfg {:email (:email info)
-                                            :backend (:backend info)
-                                            :fullname (:fullname info)})]
-        (if-let [token (:invitation-token params)]
-          ;; If invitation token comes in params, this is because the
-          ;; user comes from team-invitation process; in this case,
-          ;; regenerate token and send back to the user a new invitation
-          ;; token (and mark current session as logged).
-          (let [claims (tokens :verify {:token token :iss :team-invitation})
-                claims (assoc claims
-                              :member-id  (:id profile)
-                              :member-email (:email profile))
-                token  (tokens :generate claims)]
-            (with-meta {:invitation-token token}
-              {:transform-response ((:create session) (:id profile))
-               ::audit/props (:props profile)
-               ::audit/profile-id (:id profile)}))
-
-          (with-meta profile
-            {:transform-response ((:create session) (:id profile))
-             ::audit/props (:props profile)
-             ::audit/profile-id (:id profile)}))))))
-
-(defn- replace-several [s & {:as replacements}]
-  (reduce-kv clojure.string/replace s replacements))
-
-(defn- get-ldap-user
-  [cpool {:keys [email] :as params}]
-  (let [query   (-> (cfg/get :ldap-user-query)
-                    (replace-several ":username" email))
-
-        attrs   [(cfg/get :ldap-attrs-username)
-                 (cfg/get :ldap-attrs-email)
-                 (cfg/get :ldap-attrs-photo)
-                 (cfg/get :ldap-attrs-fullname)]
-
-        base-dn (cfg/get :ldap-base-dn)
-        params  {:filter query
-                 :sizelimit 1
-                 :attributes attrs}]
-    (first (ldap/search cpool base-dn params))))
-
-(defn- authenticate
-  [{:keys [password email] :as params}]
-  (with-open [conn (connect)]
-    (when-let [{:keys [dn] :as luser} (get-ldap-user conn params)]
-      (when (ldap/bind? conn dn password)
-        {:photo    (get luser (keyword (cfg/get :ldap-attrs-photo)))
-         :fullname (get luser (keyword (cfg/get :ldap-attrs-fullname)))
-         :email    email
-         :backend  "ldap"}))))
-
-(defn- login-or-register
-  [{:keys [conn] :as cfg} info]
-  (or (some->> (:email info)
-               (profile-q/retrieve-profile-data-by-email conn)
-               (profile-q/populate-additional-data conn)
-               (profile-q/decode-profile-row))
-      (let [params  (-> info
-                        (assoc :is-active true)
-                        (assoc :is-demo false))]
-        (->> params
-             (profile-m/create-profile conn)
-             (profile-m/create-profile-relations conn)
-             (profile-q/strip-private-attrs)))))

backend/src/app/cli/manage.clj
@@ -10,6 +10,7 @@
    [app.common.logging :as l]
    [app.db :as db]
    [app.main :as main]
+   [app.rpc.commands.auth :as cmd.auth]
    [app.rpc.mutations.profile :as profile]
    [app.rpc.queries.profile :refer [retrieve-profile-data-by-email]]
    [clojure.string :as str]
@@ -54,13 +55,13 @@
                                          :type :password}))]
     (try
       (db/with-atomic [conn (:app.db/pool system)]
-        (->> (profile/create-profile conn
+        (->> (cmd.auth/create-profile conn
                                      {:fullname fullname
                                       :email email
                                       :password password
                                       :is-active true
                                       :is-demo false})
-             (profile/create-profile-relations conn)))
+             (cmd.auth/create-profile-relations conn)))
 
       (when (pos? (:verbosity options))
         (println "User created successfully."))

backend/src/app/config.clj
@@ -79,7 +79,6 @@
    :ldap-attrs-username "uid"
    :ldap-attrs-email "mail"
    :ldap-attrs-fullname "cn"
-   :ldap-attrs-photo "jpegPhoto"
 
    ;; a server prop key where initial project is stored.
    :initial-project-skey "initial-project"})
@@ -149,7 +148,6 @@
 (s/def ::initial-project-skey ::us/string)
 (s/def ::ldap-attrs-email ::us/string)
 (s/def ::ldap-attrs-fullname ::us/string)
-(s/def ::ldap-attrs-photo ::us/string)
 (s/def ::ldap-attrs-username ::us/string)
 (s/def ::ldap-base-dn ::us/string)
 (s/def ::ldap-bind-dn ::us/string)
@@ -256,7 +254,6 @@
                    ::initial-project-skey
                    ::ldap-attrs-email
                    ::ldap-attrs-fullname
-                   ::ldap-attrs-photo
                    ::ldap-attrs-username
                    ::ldap-base-dn
                    ::ldap-bind-dn

backend/src/app/http.clj
@@ -9,7 +9,6 @@
    [app.common.data :as d]
    [app.common.logging :as l]
    [app.common.transit :as t]
-   [app.http.doc :as doc]
    [app.http.errors :as errors]
    [app.http.middleware :as middleware]
    [app.metrics :as mtx]
@@ -67,8 +66,10 @@
                  :xnio/worker-threads (:worker-threads cfg)
                  :xnio/dispatch (:executor cfg)
                  :ring/async true}
+
         handler (if (some? router)
                   (wrap-router router)
+
                   handler)
         server  (yt/server handler (d/without-nils options))]
     (assoc cfg :server (yt/start! server))))
@@ -113,7 +114,6 @@
 ;; HTTP ROUTER
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(s/def ::rpc map?)
 (s/def ::oauth map?)
 (s/def ::storage map?)
 (s/def ::assets map?)
@@ -122,15 +122,27 @@
 (s/def ::audit-handler fn?)
 (s/def ::awsns-handler fn?)
 (s/def ::session map?)
-(s/def ::debug-routes vector?)
+(s/def ::rpc-routes (s/nilable vector?))
+(s/def ::debug-routes (s/nilable vector?))
+(s/def ::oidc-routes (s/nilable vector?))
+(s/def ::doc-routes (s/nilable vector?))
 
 (defmethod ig/pre-init-spec ::router [_]
-  (s/keys :req-un [::rpc ::mtx/metrics ::ws ::oauth ::storage ::assets
-                   ::session ::feedback ::awsns-handler ::debug-routes
-                   ::audit-handler]))
+  (s/keys :req-un [::mtx/metrics
+                   ::ws
+                   ::storage
+                   ::assets
+                   ::session
+                   ::feedback
+                   ::awsns-handler
+                   ::debug-routes
+                   ::oidc-routes
+                   ::audit-handler
+                   ::rpc-routes
+                   ::doc-routes]))
 
 (defmethod ig/init-key ::router
-  [_ {:keys [ws session rpc oauth metrics assets feedback debug-routes] :as cfg}]
+  [_ {:keys [ws session metrics assets feedback] :as cfg}]
   (rr/router
    [["" {:middleware [[middleware/server-timing]
                       [middleware/format-response]
@@ -145,7 +157,7 @@
       ["/by-file-media-id/:id" {:handler (:file-objects-handler assets)}]
       ["/by-file-media-id/:id/thumbnail" {:handler (:file-thumbnails-handler assets)}]]
 
-     debug-routes
+     (:debug-routes cfg)
 
      ["/webhooks"
       ["/sns" {:handler (:awsns-handler cfg)
@@ -156,22 +168,12 @@
                            :allowed-methods #{:get}}]
 
      ["/api" {:middleware [[middleware/cors]
-                           (:middleware session)]}
-      ["/_doc" {:handler (doc/handler rpc)
-                :allowed-methods #{:get}}]
-      ["/feedback" {:handler feedback
-                    :allowed-methods #{:post}}]
-
-      ["/auth/oauth/:provider" {:handler (:handler oauth)
-                                :allowed-methods #{:post}}]
-      ["/auth/oauth/:provider/callback" {:handler (:callback-handler oauth)
-                                         :allowed-methods #{:get}}]
-
+                           [(:middleware session)]]}
       ["/audit/events" {:handler (:audit-handler cfg)
                         :allowed-methods #{:post}}]
+      ["/feedback" {:handler feedback
+                    :allowed-methods #{:post}}]
+      (:doc-routes cfg)
+      (:oidc-routes cfg)
+      (:rpc-routes cfg)]]]))
 
-      ["/rpc"
-       ["/command/:command" {:handler (:command-handler rpc)}]
-       ["/query/:type" {:handler (:query-handler rpc)}]
-       ["/mutation/:type" {:handler (:mutation-handler rpc)
-                           :allowed-methods #{:post}}]]]]]))

backend/src/app/http/doc.clj
@@ -9,14 +9,16 @@
   (:require
    [app.common.data :as d]
    [app.config :as cf]
+   [app.rpc :as-alias rpc]
    [app.util.services :as sv]
    [app.util.template :as tmpl]
    [clojure.java.io :as io]
    [clojure.spec.alpha :as s]
+   [integrant.core :as ig]
    [pretty-spec.core :as ps]
    [yetti.response :as yrs]))
 
-(defn get-spec-str
+(defn- get-spec-str
   [k]
   (with-out-str
     (ps/pprint (s/form k)
@@ -24,8 +26,8 @@
                              "clojure.core.specs.alpha" "score"
                              "clojure.core" nil}})))
 
-(defn prepare-context
-  [rpc]
+(defn- prepare-context
+  [methods]
   (letfn [(gen-doc [type [name f]]
             (let [mdata (meta f)]
               ;; (prn name mdata)
@@ -38,22 +40,32 @@
     {:command-methods
      (into []
            (map (partial gen-doc :command))
-           (->> rpc :methods :command (sort-by first)))
+           (->> methods :commands (sort-by first)))
      :query-methods
      (into []
            (map (partial gen-doc :query))
-           (->> rpc :methods :query (sort-by first)))
+           (->> methods :queries (sort-by first)))
      :mutation-methods
      (into []
            (map (partial gen-doc :mutation))
-           (->> rpc :methods :mutation (sort-by first)))}))
+           (->> methods :mutations (sort-by first)))}))
 
-(defn handler
-  [rpc]
-  (let [context (prepare-context rpc)]
-    (if (contains? cf/flags :backend-api-doc)
+(defn- handler
+  [methods]
+  (if (contains? cf/flags :backend-api-doc)
+    (let [context (prepare-context methods)]
       (fn [_ respond _]
         (respond (yrs/response 200 (-> (io/resource "api-doc.tmpl")
-                                       (tmpl/render context)))))
-      (fn [_ respond _]
-        (respond (yrs/response 404))))))
+                                       (tmpl/render context))))))
+    (fn [_ respond _]
+      (respond (yrs/response 404)))))
+
+
+(defmethod ig/pre-init-spec ::routes [_]
+  (s/keys :req-un [::rpc/methods]))
+
+(defmethod ig/init-key ::routes
+  [_ {:keys [methods] :as cfg}]
+  ["/_doc" {:handler (handler methods)
+            :allowed-methods #{:get}}])
+

commit a8f2d8e9ee9f170abb133c8dc5f47b3f43564064

:bug: Fix :hide typo dropping LDAP not-initialized error hint (#9159)
login-with-ldap raised a :restriction exception with the message
"ldap auth provider is not initialized" stored under :hide instead
of :hint. ex/raise (common/src/app/common/exceptions.cljc:33-34)
uses :hint as the ExceptionInfo message and the downstream error
formatters only read :hint (line 250, 312) — :hide is unread
anywhere in the codebase (0 other occurrences vs 447 for :hint).

Effect: when LDAP is misconfigured, operators saw the generic
"restriction" error message instead of the diagnostic string. The
typo has been present since the LDAP command was first introduced
by commit 5e3ed12686 (2022-06-30, "Refactor auth code") and was
carried forward through bdab4b020 (2023-01-05, "Fix issues on ldap
provider and rpc method") without ever surfacing as a code-review
comment.

One-character fix: :hide -> :hint. Add a CHANGES.md entry under
the 2.17.0 Unreleased :bug: Bugs fixed section.

backend/src/app/rpc/commands/ldap.clj
@@ -42,7 +42,7 @@
   (when-not provider
     (ex/raise :type :restriction
               :code :ldap-not-initialized
-              :hide "ldap auth provider is not initialized"))
+              :hint "ldap auth provider is not initialized"))
 
   (let [info (ldap/authenticate provider params)]
     (when-not info